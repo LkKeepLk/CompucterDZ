349f889dbaba316c81ccdb3cfb28ed174d75ff4d
aleks <aleks@DESKTOP-ELH28KS>
2022-10-09 20:08:06 +0300

    Добавьте файлы проекта.

---
+++ Compucter/Computer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compucter
+{
+    internal class Computer
+    {
+        private string _name;
+        private int _sizeRAM;
+        private  int _sizeHDD;
+        private bool _condition = false;
+        private string _OC;
+
+
+        public Computer(string name, int sizeRAM, string OC, int sizeHDD)
+        {
+            _name = name;
+            _sizeRAM = sizeRAM;
+            _sizeHDD = sizeHDD;
+            _OC = OC;
+
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя утройства - {_name}");
+            Console.WriteLine($"Имя системы - {_OC}");
+            Console.WriteLine($"кол-во памяти в диске - {_sizeHDD}ГБ");
+            Console.WriteLine($"кол-во оперативной памяти - {_sizeRAM}ГБ");
+        }
+        public void TurnOn()
+        {
+            _condition = true;
+            Console.WriteLine("Устройство включено");
+        }
+        public void TurnOff()
+        {
+            _condition = false;
+            Console.WriteLine("Устройство выключено");
+        }
+        public void InstallOC(string OC)
+        { _OC = OC;
+            if (_condition == false)
+            {
+                Console.WriteLine("Невозможно установить ОС при выключенном устройстве");
+            }
+            else if (_condition == true)
+            {
+                Console.WriteLine("Идет установка MacOS");
+            }
+        }
+    }
+}
---
+++ Compucter/Program.cs
@@ -0,0 +1,33 @@
+﻿using Compucter;
+
+Computer comp1 = new Computer("Кампутер", 16, "Shindoshs", 500);
+while (true)
+{
+    void Menu()
+    {
+        Console.WriteLine("1 - Включить");
+        Console.WriteLine("2 - Установить операционную систему");
+        Console.WriteLine("3 - Выключить");
+        Console.WriteLine("4 - Получить информацию");
+        string h = Console.ReadLine();
+        int k = int.Parse(h);
+        if (k == 1)
+        {
+            comp1.TurnOn();
+        }
+        if (k == 2)
+        {
+            comp1.InstallOC("MacOC");
+        }
+        if (k == 3)
+        {
+            comp1.TurnOff();
+        }
+        if (k == 4)
+        {
+            comp1.GetInfo();
+        }
+    }
+
+    Menu();
+}
